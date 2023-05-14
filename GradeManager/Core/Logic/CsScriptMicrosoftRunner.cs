062ebda17f60f7f415dc14e39985cd62fcb4065b
philippfuereder <43365816+philippfuereder@users.noreply.github.com>
2023-05-14 20:05:59 +0200

    exception handling c#-script

--- GradeManager/Core/Logic/CsScriptMicrosoftRunner.cs
+++ GradeManager/Core/Logic/CsScriptMicrosoftRunner.cs
@@ -14,22 +14,30 @@ namespace Core.Logic
         public static async Task<Grade> RunScriptAsync(GradeKey key, List<Grade> grades)
         {
             var result = new Grade();
-
             var globals = new Globals { GradeKey = key, Grades = grades };
-            var state = await CSharpScript.RunAsync(key.Calculation, globals: globals);
 
-            double resultGrade = 0.0;
-            foreach (var variable in state.Variables)
+            try
             {
-                if (variable.Name == "result")
+                var state = await CSharpScript.RunAsync(key.Calculation, globals: globals);
+
+                double resultGrade = 0.0;
+                foreach (var variable in state.Variables)
                 {
-                    resultGrade = (double)variable.Value;
+                    if (variable.Name == "result")
+                    {
+                        resultGrade = (double)variable.Value;
+                    }
                 }
+
+                result.Teacher = key.Teacher;
+                result.Graduate = Convert.ToInt32(resultGrade);
             }
-
-            result.Teacher = key.Teacher;
-            result.Graduate = Convert.ToInt32(resultGrade);
-
+            catch (Exception)
+            {
+                result.Teacher = null;
+                result.Graduate = 0;
+            }
+            
             return result;
         }
         public class Globals

829af2c3300a06d43e64b498bdae41371c219baf
philippfuereder <43365816+philippfuereder@users.noreply.github.com>
2023-05-20 22:08:18 +0200

    fixed javascriptrunner

--- GradeManager/Core/Logic/JavascriptRunner.cs
+++ GradeManager/Core/Logic/JavascriptRunner.cs
@@ -1,47 +1,40 @@
-﻿using Jint;
+﻿using JavaScriptEngineSwitcher.Jint;
+using Jint;
 using Jint.Native;
 using Jint.Native.Object;
 using Jint.Runtime;
-using Jint.Runtime.Debugger;
 using Jint.Runtime.Interop;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+using Newtonsoft.Json;
 using Shared.Entities;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.ConstrainedExecution;
 
 namespace Core.Logic
 {
     public class JavascriptRunner
     {
         public static Grade RunScript(GradeKey key, List<Grade> grades)
-        {           
-            var engine = new Engine(cfg => cfg.AllowClr());
+        {
+            var engine = new JintJsEngine();
             Grade result = new Grade();
+            List<string>? logs = new List<string>();
 
             try
             {
-                engine
-                    .SetValue("gradeKinds", key.UsedKinds.ToArray())
-                    .SetValue("grades", grades.ToArray());
+                // Definiere eine Variable im JavaScript-Code, um die console.log-Ausgaben zu speichern
+                engine.Execute("var consoleOutput = [];");
 
-<<<<<<< Updated upstream
-                // Das "console"-Objekt in der Jint-Engine erstellen
-                ObjectInstance consoleObject = engine.Object.Construct(Arguments.Empty);
-                consoleObject.FastAddProperty("log", new ClrFunctionInstance(engine, ConsoleLog), true, false, true);
-
-                // Das "console"-Objekt der Jint-Engine hinzufügen
-                engine.SetValue("console", consoleObject);
-                engine.Execute(key.Calculation);
-=======
                 // Definiere die console.log-Funktion im JavaScript-Code
-                engine.Execute(
-                    @"
+                engine.Execute(@"
                                 var console = {
                                     log: function() {
                                         consoleOutput.push(Array.from(arguments).join(' '));
                                     }
                                 };
-                            "
-                );
+                            ");
+
 
                 var gradeKindsList = JsonConvert.SerializeObject(key.UsedKinds);
                 var gradesList = JsonConvert.SerializeObject(grades);
@@ -61,19 +54,14 @@ namespace Core.Logic
                 if (jsonOutput != null)
                 {
                     logs = JsonConvert.DeserializeObject<List<string>>(jsonOutput);
-
-                    if (logs != null)
-                    {
-                        DisplayOutput(logs);
-                    }
+                    DisplayOutput(logs);
                 }
->>>>>>> Stashed changes
 
                 // Get Return from Script
-                var resultGrade = engine.GetValue("result");
-                               
+                var resultGrade = engine.GetVariableValue("result");
+
                 result.Teacher = key.Teacher;
-                result.Graduate = TypeConverter.ToInt32(resultGrade);
+                result.Graduate = Convert.ToInt32(resultGrade);
             }
             catch (Exception)
             {
@@ -82,15 +70,13 @@ namespace Core.Logic
             }
             return result;
         }
-<<<<<<< Updated upstream
-        private static JsValue ConsoleLog(JsValue thisObject, object[] arguments)
-=======
 
-        private static void DisplayOutput(List<string> logs)
->>>>>>> Stashed changes
+        private static void DisplayOutput(List<string>? logs)
         {
-            Debug.WriteLine(string.Join(',', arguments));
-            return JsValue.Undefined;
+            foreach (string output in logs)
+            {
+                Debug.WriteLine(output);
+            }
         }
     }
 }
